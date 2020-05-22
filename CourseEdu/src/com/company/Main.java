ce0c5b8341223d3ef1e34637d39a9e8e48b8d432
Agumented <Kelemu>
2020-05-22 13:45:48 +0300

    First commit

---
+++ CourseEdu/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String word1=scanner.nextLine();
+        String word2=scanner.next();
+        String line1=scanner.next();
+        System.out.println(word1);
+        System.out.println(word2);
+        System.out.println(line1);
+        System.out.println(line1);
+
+	// write your code here
+    }
+}
